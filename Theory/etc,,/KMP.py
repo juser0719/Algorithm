5d7c530e3a752675e30a24ce5ac59d566473bbb8
juser0719 <sinsa0719@naver.com>
2021-06-06 21:57:46 +0900

    KMP

---
+++ Theory/etc,,/KMP.py
@@ -0,0 +1,35 @@
+# 문자열 매칭 알고리즘
+## 단순 비교 알고리즘 보다 짧은 시간 복잡도를 지님.
+### 접두사와 접미사의 개념으로 일치하는 경우에 점프를 수행해서 연산 줄여줌
+
+def makeTable(pattern): # 실패 테이블
+    patternSize = len(pattern)
+    table = [0] * patternSize
+    j = 0
+    for i in range(1,patternSize):
+        while j > 0 and pattern[i] != pattern[j] :
+            j = table[j-1]
+        if pattern[i] == pattern[j]:
+            j += 1
+            table[i] = j
+    
+    return table # 접두사 접미사 최대 길이 테이블
+
+def KMP(parent,pattern):
+    table = makeTable(pattern)
+    parentSize = len(parent)
+    patternSize = len(pattern)
+    j = 0
+
+    for i in range(parentSize):
+        while j > 0 and parent[i] != pattern[j]:
+            j = table[j-1]
+        if parent[i] == pattern[j]:
+            if j == patternSize -1:
+                print(i-patternSize,"에서 찾음")
+                j = table[j]
+            else:
+                j+=1
+
+
+print(KMP("abacaabaabacaaba","abacaaba"))
