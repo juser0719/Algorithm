625ba419cfbb4b78637ebf0ddc2ce5be2c481873
juser0719 <sinsa0719@naver.com>
2021-08-03 21:33:19 +0900

    .

--- SWE_eclips/src/algo/달팽이숫자.java
+++ SWE_eclips/src/algo/달팽이숫자.java
@@ -3,6 +3,10 @@ package algo;
 import java.util.Scanner;
 
 public class 달팽이숫자 {
+	
+	static int[] dy = {0,1,0,-1};
+	static int[] dx = {1,0,-1,0};
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
@@ -11,28 +15,52 @@ public class 달팽이숫자 {
 		{
 			int N = sc.nextInt();
 			map= new int[N][N];
-			boolean flg = true;
-			int num = 1;
 			
-			for(int i = 0 ; i < N; i++)
+			if(N == 1)
 			{
-				for(int j = 0; j<N; j++)
+				System.out.printf("#%d\n",tc);
+				System.out.printf("1 \n");
+				continue;
+			}
+			
+			int dir = 0;
+			int num = 1;
+			int y =0, x=0;
+			
+			for(int i = 1 ; i <= N*N ; i++)
+			{
+				map[y][x] = i;
+				x += dx[dir];
+				y += dy[dir];
+				
+				if(x<0 || x>=N || y<0 || y>= N)
 				{
-					if(flg)
-					{
-						map[i][j] = num;
-						num++;
-					}else
-					{
-						map[j][i] = num;
-						num++;
-					}
-					if() flg = !flg;
+					x -=dx[dir];
+					y -=dy[dir];
+					dir = (dir+1)%4;
 					
+					x += dx[dir];
+					y += dy[dir];
+				}
+				
+				if(map[y][x] != 0)
+				{
+					x -= dx[dir];
+					y -= dy[dir];
+					dir = (dir+1)%4;
+					x += dx[dir];
+					y += dy[dir];
 				}
 			}
 			
+			System.out.printf("#%d\n",tc);
 			
+			for(int i= 0; i<N; i++)
+			{
+				for(int j = 0 ; j<N; j++)
+					System.out.printf("%d ",map[i][j]);
+				System.out.println();
+			}
 		}
 	}
 }
